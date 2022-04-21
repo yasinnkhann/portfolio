407322e67f42840560ad9d5215301eaf81330c34
Yasin Khan <yasinkhan500@gmail.com>
2022-04-20 17:13:27 -0700

    stache

--- src/pages/About.tsx
+++ src/pages/About.tsx
@@ -10,12 +10,12 @@ export default function About() {
         <title>About</title>
       </Helmet>
       <Layout>
-        <section className="mt-[calc(var(--header-height)+1rem)] font-[Manrope] grid">
-          <div>
-            My name is Yasin Khan and I&apos;m a software engineer based in the
-            San Diego County!
+        <section className="mt-[calc(var(--header-height)+1rem)] font-[Manrope] grid grid-rows-3 grid-cols-2">
+          <div className="mx-1">
+            Welcome to my page! My name is Yasin Khan and I&apos;m a software
+            engineer based in the San Diego County!
           </div>
-          <div>
+          <div className="mr-1">
             <StaticImage src="../assets/photo-in-favela.JPG" alt="me in rio" />
           </div>
         </section>
