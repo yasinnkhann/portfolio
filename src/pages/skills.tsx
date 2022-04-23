e6f5b75dc5eab6d23ad6a0093625781153bc4fd0
Yasin Khan <yasinkhan500@gmail.com>
2022-04-22 23:54:37 -0700

    stache

--- src/pages/skills.tsx
+++ src/pages/skills.tsx
@@ -1,11 +1,9 @@
 import React from 'react';
 import Layout from '@/components/Layout';
 import { Helmet } from 'react-helmet';
-import { StaticImage, GatsbyImage, getImage } from 'gatsby-plugin-image';
 import { graphql } from 'gatsby';
 
 export default function Skills({ data }) {
-  console.log(data);
   const mappedSkills = data.allFile.edges.map((edge) => (
     <div key={edge.node.id}>
       <img src={edge.node.publicURL} alt="" />
@@ -18,11 +16,6 @@ export default function Skills({ data }) {
       </Helmet>
       <Layout>
         <section className="mt-[calc(var(--header-height)+1rem)] mb-1 font-[Manrope]">
-          {/* {console.log(`../assets/${data.allFile.edges[0].node.relativePath}`)} */}
-          {/* <StaticImage
-            src={`../assets/${data.allFile.edges[0].node.relativePath}`}
-            alt=""
-          /> */}
           <div>{mappedSkills}</div>
         </section>
       </Layout>
