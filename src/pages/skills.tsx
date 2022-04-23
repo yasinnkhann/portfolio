84d0c2b90be95a5287703998f29fa75f9e4be6b9
Yasin Khan <yasinkhan500@gmail.com>
2022-04-23 16:43:41 -0700

    stache

--- src/pages/skills.tsx
+++ src/pages/skills.tsx
@@ -6,7 +6,14 @@ import { graphql } from 'gatsby';
 export default function Skills({ data }) {
   const mappedSkills = data.allFile.edges.map((edge) => (
     <div key={edge.node.id} className="w-[10rem] m-4">
-      <img src={edge.node.publicURL} alt={edge.node.name} className="w-full" />
+      <img
+        src={edge.node.publicURL}
+        alt={`${edge.node.name.split(`-`)[0]} icon'`}
+        className="w-full"
+      />
+      <p className="text-center underline my-1">
+        {edge.node.name.split(`-`)[0]}
+      </p>
     </div>
   ));
   return (
