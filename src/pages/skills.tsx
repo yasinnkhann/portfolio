e45504a78976374a7d4f3cfc42daeb4bd8398eee
Yasin Khan <yasinkhan500@gmail.com>
2022-04-23 21:17:50 -0700

    stache

--- src/pages/skills.tsx
+++ src/pages/skills.tsx
@@ -4,15 +4,25 @@ import { Helmet } from 'react-helmet';
 import { graphql } from 'gatsby';
 
 export default function Skills({ data }) {
+  const multipleWordsExceptions = {
+    'react-testing-library': true,
+  };
+
   const mappedSkills = data.allFile.edges.map((edge) => (
     <div key={edge.node.id} className="w-[10rem] m-4">
       <img
         src={edge.node.publicURL}
-        alt={`${edge.node.name.split(`-`)[0]} icon'`}
+        alt={
+          !multipleWordsExceptions[edge.node.name]
+            ? `${edge.node.name.split(`-`)[0]} icon'`
+            : `${edge.node.name.split(`-`).join(` `)} icon`
+        }
         className="w-full"
       />
       <p className="text-center underline my-1">
-        {edge.node.name.split(`-`)[0]}
+        {!multipleWordsExceptions[edge.node.name]
+          ? `${edge.node.name.split(`-`)[0]}`
+          : `${edge.node.name.split(`-`).join(` `)}`}
       </p>
     </div>
   ));
