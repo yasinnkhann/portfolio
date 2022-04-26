efc0ec80767f4589b61e4afe38422adb4066fee1
Yasin Khan <yasinkhan500@gmail.com>
2022-04-26 16:46:00 -0700

    stache

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -62,13 +62,28 @@ export default function Home({ data }) {
         <title>Home</title>
       </Helmet>
       <Layout>
-        <section className="font-[Manrope] mt-[calc(var(--header-height-mobile)+1rem)] 2xl:mt-[calc(var(--header-height-2xl)+1rem)]">
-          <h1 className="text-center">WELCOME</h1>
+        <section className="grid grid-cols-[45%_55%] grid-rows-[50vh_40vh] font-[Manrope] mt-[calc(var(--header-height-mobile)+1rem)] 2xl:mt-[calc(var(--header-height-2xl)+1rem)]">
+          <div>
+            <h1 className="text-center">WELCOME!</h1>
+            <h2>Hello, my name is Yasin and I am a software engineer</h2>
+          </div>
           <GatsbyImage
-            className="max-h-[calc(100vh-var(--header-height-mobile)-4rem)] 2xl:max-h-[calc(100vh-var(--header-height-2xl)-4rem)]"
+            className="min-w-[33rem] max-h-[calc(100vh-var(--header-height-mobile)-4rem)] 2xl:max-h-[calc(100vh-var(--header-height-2xl)-4rem)]"
             image={data.file.childImageSharp.gatsbyImageData}
             alt=""
           />
+          {/* <section className="grid grid-cols-[1fr_1fr] grid-rows-[5vh_85vh] font-[Manrope] mt-[calc(var(--header-height-mobile)+1rem)] 2xl:mt-[calc(var(--header-height-2xl)+1rem)]">
+          <h1 className="col-start-1 col-end-3 text-center">WELCOME</h1>
+          <GatsbyImage
+            className="col-start-1 col-end-3 row-start-2 row-end-3 max-h-[calc(100vh-var(--header-height-mobile)-4rem)] 2xl:max-h-[calc(100vh-var(--header-height-2xl)-4rem)]"
+            image={data.file.childImageSharp.gatsbyImageData}
+            alt=""
+          /> */}
+          {/* <GatsbyImage
+            className="max-h-[calc(100vh-var(--header-height-mobile)-4rem)] 2xl:max-h-[calc(100vh-var(--header-height-2xl)-4rem)]"
+            image={data.file.childImageSharp.gatsbyImageData}
+            alt=""
+          /> */}
         </section>
         {/* <section className="font-[Manrope] mt-[calc(var(--header-height-mobile))] bg-[url('../assets/home/leader.png')] bg-no-repeat bg-[length:100%] h-[calc(100vh-var(--header-height-mobile))]">
           <h1 className="text-center">WELCOME</h1>
@@ -84,7 +99,7 @@ export default function Home({ data }) {
 
 export const query = graphql`
   query Banner {
-    file(relativePath: { eq: "home/splash2.png" }) {
+    file(relativePath: { eq: "home/splash.png" }) {
       id
       name
       childImageSharp {
