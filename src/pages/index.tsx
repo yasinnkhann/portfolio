6e4e537d185a9c6055cc2566eb45cfec949ab4ff
Yasin Khan <yasinkhan500@gmail.com>
2022-04-27 14:44:23 -0700

    stache

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -78,26 +78,28 @@ export default function Home({ data }) {
             </h1>
           </div>
           <GatsbyImage
-            className="min-w-[33rem] min-h-[21rem] max-h-[calc(100vh-var(--header-height-mobile)-4rem)] 2xl:max-h-[calc(100vh-var(--header-height-2xl)-4rem)]"
+            className="mx-auto my-auto"
             image={data.file.childImageSharp.gatsbyImageData}
             alt=""
           />
-          {/* <section className="grid grid-cols-[1fr_1fr] grid-rows-[5vh_85vh] font-[Manrope] mt-[calc(var(--header-height-mobile)+1rem)] 2xl:mt-[calc(var(--header-height-2xl)+1rem)]">
-          <h1 className="col-start-1 col-end-3 text-center">WELCOME</h1>
-          <GatsbyImage
-            className="col-start-1 col-end-3 row-start-2 row-end-3 max-h-[calc(100vh-var(--header-height-mobile)-4rem)] 2xl:max-h-[calc(100vh-var(--header-height-2xl)-4rem)]"
-            image={data.file.childImageSharp.gatsbyImageData}
-            alt=""
-          /> */}
-          {/* <GatsbyImage
-            className="max-h-[calc(100vh-var(--header-height-mobile)-4rem)] 2xl:max-h-[calc(100vh-var(--header-height-2xl)-4rem)]"
-            image={data.file.childImageSharp.gatsbyImageData}
-            alt=""
-          /> */}
         </section>
-        {/* <section className="font-[Manrope] mt-[calc(var(--header-height-mobile))] bg-[url('../assets/home/leader.png')] bg-no-repeat bg-[length:100%] h-[calc(100vh-var(--header-height-mobile))]">
-          <h1 className="text-center">WELCOME</h1>
+        {/* <section className="grid grid-cols-[45%_55%] grid-rows-[50vh_40vh] font-[Manrope] mt-[calc(var(--header-height-mobile)+1rem)] 2xl:mt-[calc(var(--header-height-2xl)+1rem)]">
+          <div className="relative row-start-1 row-end-3">
+            <h1 className="absolute top-[25%] left-[10%]">
+              Hello, my name is{` `}
+              <strong className="text-red-500 hover:animate-pulse">
+                Yasin
+              </strong>
+              {` `}
+              and I am a
+              <br />
+              <strong className="text-blue-500 hover:animate-pulse">
+                software engineer.
+              </strong>
+            </h1>
+          </div>
           <GatsbyImage
+            className="min-w-[33rem] min-h-[21rem] max-h-[calc(100vh-var(--header-height-mobile)-4rem)] 2xl:max-h-[calc(100vh-var(--header-height-2xl)-4rem)]"
             image={data.file.childImageSharp.gatsbyImageData}
             alt=""
           />
