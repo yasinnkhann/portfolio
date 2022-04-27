f5fb1ed8d7cc3e775667576505c3d3c74019e2d0
Yasin Khan <yasinkhan500@gmail.com>
2022-04-26 18:58:51 -0700

    stache

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -63,12 +63,22 @@ export default function Home({ data }) {
       </Helmet>
       <Layout>
         <section className="grid grid-cols-[45%_55%] grid-rows-[50vh_40vh] font-[Manrope] mt-[calc(var(--header-height-mobile)+1rem)] 2xl:mt-[calc(var(--header-height-2xl)+1rem)]">
-          <div>
-            <h1 className="text-center">WELCOME!</h1>
-            <h2>Hello, my name is Yasin and I am a software engineer</h2>
+          <div className="relative row-start-1 row-end-3">
+            <h1 className="absolute top-[25%] left-[10%]">
+              Hello, my name is{` `}
+              <strong className="text-red-500 hover:animate-pulse">
+                Yasin
+              </strong>
+              {` `}
+              and I am a
+              <br />
+              <strong className="text-blue-500 hover:animate-pulse">
+                software engineer.
+              </strong>
+            </h1>
           </div>
           <GatsbyImage
-            className="min-w-[33rem] max-h-[calc(100vh-var(--header-height-mobile)-4rem)] 2xl:max-h-[calc(100vh-var(--header-height-2xl)-4rem)]"
+            className="min-w-[33rem] min-h-[21rem] max-h-[calc(100vh-var(--header-height-mobile)-4rem)] 2xl:max-h-[calc(100vh-var(--header-height-2xl)-4rem)]"
             image={data.file.childImageSharp.gatsbyImageData}
             alt=""
           />
