ebb7c51507904cac93c303566ca9dae1700ed260
Yasin Khan <yasinkhan500@gmail.com>
2022-04-27 16:38:13 -0700

    stache

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -62,9 +62,9 @@ export default function Home({ data }) {
         <title>Home</title>
       </Helmet>
       <Layout>
-        <section className="grid grid-cols-[45%_55%] grid-rows-[50vh_40vh] font-[Manrope] mt-[calc(var(--header-height-mobile)+1rem)] 2xl:mt-[calc(var(--header-height-2xl)+1rem)]">
-          <div className="relative row-start-1 row-end-3">
-            <h1 className="absolute top-[25%] left-[10%]">
+        <section className="grid grid-cols-[45%_55%] grid-rows-[40vh_50vh] sm:items-center font-[Manrope] mt-[calc(var(--header-height-mobile)+1rem)] 2xl:mt-[calc(var(--header-height-2xl)+1rem)]">
+          <div className="row-start-1 row-end-2 col-start-1 col-end-3 mt-4 sm:row-start-1 sm:col-end-2">
+            <h1 className="text-center">
               Hello, my name is{` `}
               <strong className="text-red-500 hover:animate-pulse">
                 Yasin
@@ -78,32 +78,11 @@ export default function Home({ data }) {
             </h1>
           </div>
           <GatsbyImage
-            className="mx-auto my-auto"
+            className="mx-auto my-auto row-start-2 row-end-3 col-start-1 col-end-3 sm:row-start-1 sm:row-end-2 sm:col-start-2"
             image={data.file.childImageSharp.gatsbyImageData}
             alt=""
           />
         </section>
-        {/* <section className="grid grid-cols-[45%_55%] grid-rows-[50vh_40vh] font-[Manrope] mt-[calc(var(--header-height-mobile)+1rem)] 2xl:mt-[calc(var(--header-height-2xl)+1rem)]">
-          <div className="relative row-start-1 row-end-3">
-            <h1 className="absolute top-[25%] left-[10%]">
-              Hello, my name is{` `}
-              <strong className="text-red-500 hover:animate-pulse">
-                Yasin
-              </strong>
-              {` `}
-              and I am a
-              <br />
-              <strong className="text-blue-500 hover:animate-pulse">
-                software engineer.
-              </strong>
-            </h1>
-          </div>
-          <GatsbyImage
-            className="min-w-[33rem] min-h-[21rem] max-h-[calc(100vh-var(--header-height-mobile)-4rem)] 2xl:max-h-[calc(100vh-var(--header-height-2xl)-4rem)]"
-            image={data.file.childImageSharp.gatsbyImageData}
-            alt=""
-          />
-        </section> */}
       </Layout>
     </>
   );
