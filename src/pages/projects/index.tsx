0a8a7c8e79026e2c63bcd46f56ab1f15561dba27
Yasin Khan <yasinkhan500@gmail.com>
2022-04-30 00:43:43 -0700

    stache

--- src/pages/projects/index.tsx
+++ src/pages/projects/index.tsx
@@ -15,7 +15,7 @@ export default function Projects({ data }) {
         <section className="mt-[calc(var(--header-height-mobile)+1rem)] font-[Manrope] 2xl:mt-[calc(var(--header-height-2xl)+1rem)] 2xl:text-2xl">
           <div className="grid grid-rows-2 grid-cols-2 justify-items-center items-end">
             {projects.map(({ node: project }) => (
-              <Link to={project.slug} className="">
+              <Link to={project.slug}>
                 <div key={project.id} className="p-4 lg:p-8">
                   <GatsbyImage
                     className="border-[3px] border-orange-300 rounded-lg hover:border-orange-600"
