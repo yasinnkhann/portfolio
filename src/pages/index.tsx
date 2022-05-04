851552a09694d08fbcc8cdd8e1d76c1f9d0e8aa6
Yasin Khan <yasinkhan500@gmail.com>
2022-05-03 19:29:15 -0700

    stache

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -3,7 +3,7 @@ import Layout from '@/components/Layout';
 import { Helmet } from 'react-helmet';
 import confetti from 'canvas-confetti';
 import axios from 'axios';
-import { graphql } from 'gatsby';
+import { graphql, Link } from 'gatsby';
 import { GatsbyImage } from 'gatsby-plugin-image';
 
 export default function Home({ data }) {
@@ -64,7 +64,7 @@ export default function Home({ data }) {
       <Layout>
         <section className="grid grid-cols-[45%_55%] grid-rows-[40vh_50vh] sm:items-center font-[Manrope] mt-[calc(var(--header-height-mobile)+1rem)] 2xl:mt-[calc(var(--header-height-2xl)+1rem)]">
           <div className="row-start-1 row-end-2 col-start-1 col-end-3 mt-4 sm:row-start-1 sm:col-end-2">
-            <h1 className="text-center">
+            <h1 className="text-center leading-10">
               Hello, my name is{` `}
               <strong className="text-red-500 hover:animate-pulse">
                 Yasin
@@ -76,6 +76,17 @@ export default function Home({ data }) {
                 software engineer.
               </strong>
             </h1>
+            <div className="text-center mt-[5.5rem]">
+              <h2>Check out some of my work.</h2>
+              <Link to="/projects">
+                <button
+                  className="bg-black p-3 mt-4 rounded-2xl text-white text-lg hover:text-yellow-300"
+                  type="button"
+                >
+                  My Projects
+                </button>
+              </Link>
+            </div>
           </div>
           <GatsbyImage
             className="mx-auto my-auto row-start-2 row-end-3 col-start-1 col-end-3 sm:row-start-1 sm:row-end-2 sm:col-start-2"
