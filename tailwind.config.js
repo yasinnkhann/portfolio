6b64b92c1f54bea22bd82b2614ea4bb5afba1225
Yasin Khan <yasinkhan500@gmail.com>
2022-07-22 13:44:42 -0700

    got rid of moving skills

--- tailwind.config.js
+++ tailwind.config.js
@@ -13,47 +13,47 @@ module.exports = {
           '0%, 100%': { transform: 'rotate(-3deg)' },
           '50%': { transform: 'rotate(3deg)' },
         },
-        runSM: {
-          '0%, 100%': { left: '0' },
-          '50%': {
-            left: 'calc(100vw - ((var(--skill-photo-size-sm)) * 5.3))',
-          },
-        },
-        runMD: {
-          '0%, 100%': { left: '0' },
-          '50%': {
-            left: 'calc(100vw - ((var(--skill-photo-size-md)) * 4.8))',
-          },
-        },
-        runLG: {
-          '0%, 100%': { left: '0' },
-          '50%': {
-            left: 'calc(100vw - ((var(--skill-photo-size-lg)) * 6.3))',
-          },
-        },
-        runXL: {
-          '0%, 100%': { left: '0' },
-          '50%': {
-            left: 'calc(100vw - ((var(--skill-photo-size-xl)) * 6))',
-          },
-        },
-        run2XL: {
-          '0%, 100%': { left: '0' },
-          '50%': {
-            left: 'calc(100vw - ((var(--skill-photo-size-2xl)) * 5.5))',
-          },
-        },
+        // runSM: {
+        //   '0%, 100%': { left: '0' },
+        //   '50%': {
+        //     left: 'calc(100vw - ((var(--skill-photo-size-sm)) * 5.3))',
+        //   },
+        // },
+        // runMD: {
+        //   '0%, 100%': { left: '0' },
+        //   '50%': {
+        //     left: 'calc(100vw - ((var(--skill-photo-size-md)) * 4.8))',
+        //   },
+        // },
+        // runLG: {
+        //   '0%, 100%': { left: '0' },
+        //   '50%': {
+        //     left: 'calc(100vw - ((var(--skill-photo-size-lg)) * 6.3))',
+        //   },
+        // },
+        // runXL: {
+        //   '0%, 100%': { left: '0' },
+        //   '50%': {
+        //     left: 'calc(100vw - ((var(--skill-photo-size-xl)) * 6))',
+        //   },
+        // },
+        // run2XL: {
+        //   '0%, 100%': { left: '0' },
+        //   '50%': {
+        //     left: 'calc(100vw - ((var(--skill-photo-size-2xl)) * 5.5))',
+        //   },
+        // },
         moveClouds: {
           '0%': { marginLeft: '1000px' },
           '100%': { marginLeft: '-1000px' },
         },
       },
       animation: {
-        runSM: 'runSM 20s linear infinite',
-        runMD: 'runMD 20s linear infinite',
-        runLG: 'runLG 20s linear infinite',
-        runXL: 'runXL 20s linear infinite',
-        run2XL: 'run2XL 20s linear infinite',
+        // runSM: 'runSM 20s linear infinite',
+        // runMD: 'runMD 20s linear infinite',
+        // runLG: 'runLG 20s linear infinite',
+        // runXL: 'runXL 20s linear infinite',
+        // run2XL: 'run2XL 20s linear infinite',
         'spin-slow': 'spin 3s linear infinite',
       },
     },
